e76917941f5d0052f1fba2309ef309d1594cd775
Aria Hemin <ariahemin@gmail.com>
2022-11-16 19:42:38 +0300

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <TodoList />
+</template>
+
+<script>
+import TodoList from './components/todoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/components/todoList.vue
@@ -0,0 +1,68 @@
+<template>
+    <div id="task-add" >
+        <input  v-model="text" ><button @click="items.push({title: text, id: id++}), text = ''">add</button>
+    </div>
+    <div id="task-place">
+        <li id="tasks-items" v-for="(item, index) in items" v-bind:key="index">
+        <div class="task-itself" > <div>{{item.id}} {{ item.title }}</div> <span><button @click="removeTask(index)"> x</button></span></div>
+        </li>   
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return { 
+            items: [],
+            text: '',
+            id: 0,
+        }
+    },
+    methods: {
+        removeTask(index){
+            this.items = this.items.filter((task, i) => i != index);
+        }
+    }
+}
+
+</script>
+
+<style>
+#tasks-items{        
+    list-style: none;
+}
+
+#task-place{
+    text-align: left;
+    margin-inline: 20vw;
+
+}
+
+#task-place button{
+    background-color: rgb(255, 185, 185);
+    border: 0;
+    border-radius: 18px;
+}
+
+.task-itself{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+}
+
+#task-add input{
+    padding: 1px 5px;
+    border-radius: 18px;
+    border: solid 1px;
+}
+
+#task-add button{
+    background-color: white;
+    border: solid 1px;
+    border-radius: 18px;
+}
+
+
+
+</style>
